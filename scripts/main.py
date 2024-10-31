5dde2befbc24501638bc5f594108f628345b8733
mhtozbnm <mhtozbnm@gmail.com>
2024-11-01 00:16:48 +0300

    update comment line

--- scripts/main.py
+++ scripts/main.py
@@ -7,10 +7,6 @@ def main():
     df = data_extractor.get_data()
     print(df.head())  # Veri kontrolü için ilk birkaç satırı yazdırabilirsiniz
 
-    # İsterseniz burada df'yi kaydedebilir veya sonraki işlemlere aktarabilirsiniz
-    # Örneğin, df'yi CSV olarak kaydetmek:
-    # df.to_csv('data/processed/spotify_data.csv', index=False)
-
     
 
 if __name__ == '__main__':
