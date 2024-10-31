c0946ccec7893bd8c1929cb790e0de278115a4a2
mhtozbnm <mhtozbnm@gmail.com>
2024-11-01 00:38:09 +0300

    u 2

--- scripts/data_loading.py
+++ scripts/data_loading.py
@@ -1,5 +1,13 @@
 # data_loading.py
 
 
-        
+import sys
+import os
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(project_root)
+
+from sqlalchemy import create_engine
+
+
     
